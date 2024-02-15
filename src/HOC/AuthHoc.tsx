05dfb349446a5f523a4225d90c5410181bf83258
Nikhil <lucifer@Nikhils-MacBook-Air.local>
2024-02-15 17:45:41 +0530

    update session if user details not present

--- src/HOC/AuthHoc.tsx
+++ src/HOC/AuthHoc.tsx
@@ -4,12 +4,19 @@ import FullScreenLoader from "@/components/FullScreenLoader/FullScreenLoader";
 import { Page_Type } from "@/utils/constants";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export function AuthHoc(Component: any, PageType = Page_Type.auth) {
   return function AuthHoc(props: any) {
-    const { status } = useSession();
+    const { data, status, update }: any = useSession();
     console.log("🚀 ~ AuthHoc ~ status:", status);
     const router = useRouter();
+
+    useEffect(() => {
+      if (status === "authenticated" && !data?.user?.details?.id) {
+        update();
+      }
+    }, [status, data]);
 
     if (status === "unauthenticated" && PageType === Page_Type.auth) {
       router.push("/login");
