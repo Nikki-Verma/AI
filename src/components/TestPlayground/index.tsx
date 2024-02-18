0775a3b5c94aaa4b2593abbccda723eb1e921798
Nikhil <lucifer@Nikhils-MacBook-Air.local>
2024-02-18 10:32:12 +0530

    test layground changes

--- src/components/TestPlayground/index.tsx
+++ src/components/TestPlayground/index.tsx
@@ -1,6 +1,6 @@
 import useChatStream from "@/Hooks/useChatStream";
 import { UnknownObject } from "@/utils/types";
-import { FormInstance } from "antd";
+import { FormInstance, Spin } from "antd";
 import { useEffect, useState } from "react";
 import ChatBot from "../ChatBot";
 import PlaygroundConfig from "../PlaygroundConfig";
@@ -14,6 +14,12 @@ type TestPlaygroundProps = {
 
 const TestPlayground = ({ details, form, onFininsh }: TestPlaygroundProps) => {
   console.log("🚀 ~ TestPlayground ~ details:", details);
+  const [playgroundConfigDetails, setPlaygroundConfigDetails] = useState({
+    model_paramters: {},
+    kb_parameters: {},
+  });
+  const [configDetailsLoading, setConfigDetailsLoading] = useState(true);
+
   const [conversationId, setConversationId] = useState<string | undefined>();
   const {
     messages,
@@ -36,23 +42,44 @@ const TestPlayground = ({ details, form, onFininsh }: TestPlaygroundProps) => {
   useEffect(() => {
     changeConversation(conversationId);
   }, [conversationId]);
+
+  useEffect(() => {
+    const modelParameters =
+      details?.result?.model_detail?.model_parameters || {};
+    const kbParameters = details?.result?.kb?.kb_paramters || {};
+
+    const newConfigDetails = {
+      model_paramters: modelParameters,
+      kb_parameters: kbParameters,
+    };
+
+    setPlaygroundConfigDetails(newConfigDetails);
+    setConfigDetailsLoading(false);
+  }, [details]);
+
   return (
-    <TestPlaygroundContainer>
-      <div style={{ height: "100%", flex: 1, padding: "20px" }}>
-        <ChatBot
-          messages={messages}
-          changeConversationLoading={changeConversationLoading}
-          handleSubmit={handleSubmit}
-          handleInputChange={handleInputChange}
-          input={input}
-          setInput={setInput}
-          isLoading={isLoading}
-        />
-      </div>
-      <div style={{ height: "100%", width: "25%" }}>
-        <PlaygroundConfig />
-      </div>
-    </TestPlaygroundContainer>
+    <Spin spinning={configDetailsLoading}>
+      <TestPlaygroundContainer>
+        <div style={{ height: "100%", flex: 1, padding: "20px" }}>
+          <ChatBot
+            messages={messages}
+            changeConversationLoading={changeConversationLoading}
+            handleSubmit={handleSubmit}
+            handleInputChange={handleInputChange}
+            input={input}
+            setInput={setInput}
+            isLoading={isLoading}
+          />
+        </div>
+        <div style={{ height: "100%", width: "25%" }}>
+          <PlaygroundConfig
+            playgroundConfigDetails={playgroundConfigDetails}
+            canLaunch={true}
+            form={form}
+          />
+        </div>
+      </TestPlaygroundContainer>
+    </Spin>
   );
 };
 
