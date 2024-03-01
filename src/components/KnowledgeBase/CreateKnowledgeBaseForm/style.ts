b94e01d6b4280a2b6aa11dd3208548529f458252
Nikhil <lucifer@Nikhils-MacBook-Air.local>
2024-03-01 22:21:33 +0530

    kb changes

--- src/components/KnowledgeBase/CreateKnowledgeBaseForm/style.ts
+++ src/components/KnowledgeBase/CreateKnowledgeBaseForm/style.ts
@@ -1,6 +1,8 @@
 import { PRIMARY_BRAND_COLOR } from "@/_utils/theme.antd";
-import { Card, CardProps } from "antd";
+import { Card, CardProps, Typography } from "antd";
 import { styled } from "styled-components";
+
+const { Text } = Typography;
 
 interface RadioOptionContainerProps extends CardProps {
   checked: boolean;
@@ -18,4 +20,14 @@ export const RadioOptionContainer = styled(Card)<RadioOptionContainerProps>`
     }
   }};
   margin: 6px 0 !important;
+`;
+
+export const KbSettingsLabel = styled(Text)`
+  font-feature-settings:
+    "clig" off,
+    "liga" off;
+  text-overflow: ellipsis;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
 `;
