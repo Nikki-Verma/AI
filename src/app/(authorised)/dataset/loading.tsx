92f163ebd9d41f921cec9af910b4f4cb3a824ce3
Nikhil <lucifer@Nikhils-MacBook-Air.local>
2024-03-04 14:38:55 +0530

    added loading ui for all pages to test on production build

---
+++ src/app/(authorised)/dataset/loading.tsx
@@ -0,0 +1,8 @@
+import ProgressLoader from "@/components/ProgressLoader";
+import React from "react";
+
+const loading: React.FC = () => {
+  return <ProgressLoader />;
+};
+
+export default loading;
