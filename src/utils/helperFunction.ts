58cc4f982287a1c16b55f197aa5c0b3d00122ebf
Nikhil <lucifer@ip-192-168-5-105.ap-south-1.compute.internal>
2024-03-07 17:45:01 +0530

    added fallback message in geterror from api

--- src/utils/helperFunction.ts
+++ src/utils/helperFunction.ts
@@ -26,11 +26,16 @@ export const getErrorFromApi = (
     return res;
   }
   if (res?.response) {
-    const error = res?.response?.data?.detail
-      ? res?.response?.data?.detail
-      : res?.response?.data?.message
-        ? res?.response?.data?.message
-        : res?.response?.data?.error?.message;
+    const error =
+      res?.response?.data?.detail &&
+      typeof res?.response?.data?.detail === "string"
+        ? res?.response?.data?.detail
+        : res?.response?.data?.detail?.[0]?.msg &&
+            typeof res?.response?.data?.detail?.[0]?.msg === "string"
+          ? res?.response?.data?.detail?.[0]?.msg
+          : res?.response?.data?.message
+            ? res?.response?.data?.message
+            : res?.response?.data?.error?.message;
     if (error) return error;
   }
 
@@ -40,14 +45,6 @@ export const getErrorFromApi = (
   }
 
   return res?.message || defaultMessage;
-};
-
-export const getFilters = (filters: any) => {
-  const filtersObj: any = {};
-  Object.entries(filters).forEach(([key, value]: any) => {
-    filtersObj[key] = value?.[0];
-  });
-  return filtersObj;
 };
 
 // Find a way to use the meta deta content and normal content
